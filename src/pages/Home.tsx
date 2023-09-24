6870a9d8ef610b15855ba68baf36f474a96e3663
sampod76 <108483839+sampod76@users.noreply.github.com>
2023-09-25 03:14:09 +0600

    modifi home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import Books from "../components/BooksList";
+
+const Home = () => {
+  return (
+    <div className="mt-[4rem] bg-slate-400 min-h-screen">
+      <Books />
+    </div>
+  );
+};
+
+export default Home;
+
+
+
+
+
