b690298955b920659491197f4caf5d68a4571fc3
sampod76 <108483839+sampod76@users.noreply.github.com>
2023-09-25 03:14:49 +0600

    include mainlayout

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../shared/Navbar";
+import Footer from "../shared/Footer";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar />
+      <section className="mt-[4rem] ">
+        <Outlet></Outlet>
+      </section>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
